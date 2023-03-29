c376bb62af91e931cbc76f210f89abc7a8602f47
Breno Sonda <breno26sonda@gmail.com>
2023-03-29 14:42:22 -0300

    Button carregar mais added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,9 @@ export default function Home(): JSX.Element {
       <Header />
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage &&
+          <Button mt={'40px'} onClick={() => fetchNextPage()}>{isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}</Button>
+        }
       </Box>
     </>
   );
